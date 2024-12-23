9783004e8c87aa4ee8dc892aca0b76b62fca4cd0
Yanis <skyhard94@gmail.com>
2024-12-23 21:33:30 +0100

    initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        <a className="lg:hidden flex-1 cursor-pointer z-2">
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
