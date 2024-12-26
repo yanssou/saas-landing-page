35c35f32ee67f40c5f68be3ba16419809cb1cca3
yanssou <skyhard94@gmail.com>
2024-12-26 21:54:39 +0100

    added responsive management for header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,11 @@
+import { Link as LinkScroll } from "react-scroll";
+
+const NavLink = ({ title }: { title: string }) => (
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
+
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
@@ -5,6 +13,35 @@ const Header = () => {
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
+
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
+                <li className="nav-logo">
+                  <LinkScroll>
+                    <img
+                      src="/images/xora.svg"
+                      width={160}
+                      height={55}
+                      alt="logo"
+                    />
+                  </LinkScroll>
+                </li>
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </header>
   );
